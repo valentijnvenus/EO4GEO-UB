ca88efd814ee2cf098979f2464ee852cb3885c89
Claudia Perez <al390155@uji.es>
2020-03-25 17:15:46 +0100

    changed the conversion method

--- src/app/services/fileUploadService.service.ts
+++ src/app/services/fileUploadService.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable , throwError} from 'rxjs';
 import { HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
-import {catchError} from "rxjs/operators";
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,87 +10,46 @@ import {catchError} from "rxjs/operators";
 export class FileUploadServiceService {
   constructor(private http: HttpClient) { }
 
-  public  URL_BASE = 'https://eo4geo-uji.firebaseio.com/';
+  public URL_BASE = 'https://eo4geo-uji.firebaseio.com/';
   public resp = {};
-  uploadFile(file : any, user : any){
+  uploadFile(file: any, user: any) {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json'
       })
     };
-    let newFile = this.convertFile(file)
-    let fileToSave = JSON.stringify(newFile);
     return this.currentVersion().pipe(
       catchError(this.handleError)
-    ).subscribe( (cversion)=> {
-      let newVersion  = (parseInt(cversion) + 1).toString();
-      newFile.version = newVersion;
-      let currentFile = JSON.stringify(newFile);
-      let configUrl = this.URL_BASE + 'v' + newVersion + '.json?auth=' + user;
-      let currentUrl = this.URL_BASE + 'current.json?auth=' + user;
-
-      this.http.put(configUrl, fileToSave, httpOptions).pipe(
-        catchError(this.handleError)
-      ).subscribe(
-        res => this.resp = res,
-        err => this.resp = err,
-      );
-      this.http.put(currentUrl, currentFile, httpOptions).pipe(
-        catchError(this.handleError)
-      ).subscribe(
-        res => this.resp = res,
-        err => this.resp = err,
-      );
+    ).subscribe( (cversion) => {
+          const fileToSave = JSON.stringify(file);
+          const newVersion = (parseInt(cversion) + 1).toString();
+          file.version = newVersion;
+          file.updateDate = new Date();
+          this.resp = file;
+          const currentFile = JSON.stringify(file);
+          const configUrl = this.URL_BASE + 'v' + newVersion + '.json?auth=' + user;
+          const currentUrl = this.URL_BASE + 'current.json?auth=' + user;
+          this.http.put(configUrl, fileToSave, httpOptions).pipe(
+             catchError(this.handleError)
+           ).subscribe(
+             res => this.resp = res,
+             err => this.resp = err,
+           );
+           this.http.put(currentUrl, currentFile, httpOptions).pipe(
+             catchError(this.handleError)
+           ).subscribe(
+             res => this.resp = res,
+             err => this.resp = err,
+           );
     },
-      err => this.resp = err );
-
+      err =>  this.resp = err );
   }
 
-  //Get current version
-  currentVersion (): Observable<any> {
+  // Get current version
+  currentVersion(): Observable<any> {
     return  this.http.get( this.URL_BASE + 'current/version.json');
   }
-
-  convertFile ( file : any ) : any{
-    let fileToSave = {"concepts":[] , "relations":[] , "references":[] , "skills":[] };
-    let obj = JSON.parse(file);
-
-    Object.keys(obj["nodes"]).forEach( k => {
-      fileToSave.concepts.push({
-        "code" : obj["nodes"][k].label.split(']', 1)[0].split('[', 2)[1],
-        "name" : obj["nodes"][k].label.split(']')[1],
-        "description" : obj["nodes"][k].definition
-      });
-    });
-
-    Object.keys(obj["links"]).forEach( k => {
-      fileToSave.relations.push({
-        "target" : obj["links"][k].target,
-        "source" : obj["links"][k].source,
-        "name" : obj["links"][k].relationName
-      });
-    });
-    Object.keys(obj["external_resources"]).forEach( k => {
-      fileToSave.references.push({
-        "concepts" : obj["external_resources"][k].nodes,
-        "name" : obj["external_resources"][k].title,
-        "description" : obj["external_resources"][k].description,
-        "url" : obj["external_resources"][k].url
-      });
-    });
-
-    Object.keys(obj["learning_outcomes"]).forEach( k => {
-      fileToSave.skills.push({
-        "concepts":obj["learning_outcomes"][k].nodes,
-        "name": obj["learning_outcomes"][k].name,
-      });
-    });
-
-    return fileToSave;
-
-  }
-  handleError(error: HttpErrorResponse){
-    console.log("lalalalalalalala");
+  handleError(error: Error) {
     return throwError(error);
   }
 
--- src/app/views/upload/upload.component.ts
+++ src/app/views/upload/upload.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy, Input, ViewChild } from '@angular/core';
 import { FileUploadServiceService } from '../../services/fileUploadService.service';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { catchError } from "rxjs/operators";
 
 @Component({
   selector: 'app-upload',
@@ -14,13 +13,13 @@ export class UploadComponent {
   public successUpload = true;
   isAnonymous = null;
   ownUsrId = null;
+  public errorText = '';
   public errorUpload = true;
 
   @ViewChild('dangerModal') public dangerModal: ModalDirective;
 
   constructor(private fileUploadService: FileUploadServiceService, public afAuth: AngularFireAuth) {
     this.afAuth.auth.onAuthStateChanged(user => {
-      console.log(user);
       if (user) {
         this.isAnonymous = user.isAnonymous;
         this.ownUsrId = user.uid;
@@ -32,22 +31,145 @@ export class UploadComponent {
   }
 
   uploadFile( jsonInput: any) {
-    const file: File = jsonInput.target.files[0];
-    let fileReader = new FileReader();
-    let fileService = this.fileUploadService;
-    fileReader.onload = (e) => {
-      this.afAuth.auth.currentUser.getIdToken( true).then((idToken) => {
-        try {
-          fileService.uploadFile(fileReader.result, idToken);
-          this.successUpload = false;
-          this.errorUpload = true;
-        }
-       catch{
-         this.errorUpload = false;
-         this.successUpload = true;
-       }
-      });
+    try {
+      const file: File = jsonInput.target.files[0];
+      const fileReader = new FileReader();
+      const fileService = this.fileUploadService;
+      if ( file.type === 'application/json') {
+        fileReader.onload = (e) => {
+          this.afAuth.auth.currentUser.getIdToken( true).then((idToken) => {
+            const newFile = this.convertFile(fileReader.result);
+            if ( !(newFile.hasOwnProperty('Error') )) {
+              fileService.uploadFile(newFile, idToken);
+              this.errorUpload = true;
+              this.successUpload = false;
+            } else {
+              this.errorText = newFile['Error'];
+              this.errorUpload = false;
+              this.successUpload = true;
+            }
+          });
+        };
+        fileReader.readAsText(file);
+      } else {
+        this.errorText = 'The format is not valid, it is only accepted .json files';
+        this.errorUpload = false;
+        this.successUpload = true;
+      }
+    } catch (e) {
+      this.errorUpload = false;
+      this.successUpload = true;
+      this.errorText = e;
     }
-    fileReader.readAsText(file);
+  }
+  convertFile( file: any ): any {
+    const fileToSave = {'concepts': [] , 'relations': [] , 'references': [] , 'skills': [] };
+    const obj = JSON.parse(file);
+    if ( obj.hasOwnProperty('nodes')  ) {
+      fileToSave.concepts[0] = {};
+      Object.keys(obj['nodes']).forEach( k => {
+        if (obj['nodes'][k].label === '[GIST] Geographic Information Science and Technology' && obj['nodes'][k].numberOfLinks > 0 ) {
+          fileToSave.concepts[0] = {
+            'code' : obj['nodes'][k].label.split(']', 1)[0].split('[', 2)[1] ,
+            'name' : obj['nodes'][k].label.split(']')[1].trim(),
+            'description' : obj['nodes'][k].definition
+          };
+        } else {
+          fileToSave.concepts.push({
+            'code' : (obj['nodes'][k].label.split(']', 1)[0].split('[', 2)[1] != null &&
+              obj['nodes'][k].label.split(']', 1)[0].split('[', 2)[1].length > 0) ?
+              obj['nodes'][k].label.split(']', 1)[0].split('[', 2)[1] : ' ',
+            'name' : ( obj['nodes'][k].label.split(']')[1] != null && obj['nodes'][k].label.split(']')[1].length > 0 ) ?
+              obj['nodes'][k].label.split(']')[1].trim() : ' ',
+            'description' : ( obj['nodes'][k].definition != null && obj['nodes'][k].definition.length > 0 ) ?
+              obj['nodes'][k].definition : ' '
+          });
+        }
+      });
+    } else {
+      return {'Error' : 'Invalid Format'};
+    }
+    if (  obj.hasOwnProperty('links') ) {
+      Object.keys(obj['links']).forEach( k => {
+        if ( obj['links'][k].target === 330 && obj['links'][k].source <= 330 ) {
+          fileToSave.relations.push({
+            'target' : 0 ,
+            'source' : obj['links'][k].source != null ? obj['links'][k].source + 1 : ' ',
+            'name' : (obj['links'][k].relationName != null && obj['links'][k].relationName.length > 0 ) ? obj['links'][k].relationName : ' '
+          });
+        } else if ( obj['links'][k].source === 330 && obj['links'][k].target <= 330) {
+          fileToSave.relations.push({
+            'target' : obj['links'][k].target != null ? obj['links'][k].target + 1 : ' ',
+            'source' : 0 ,
+            'name' : (obj['links'][k].relationName != null && obj['links'][k].relationName.length > 0 ) ? obj['links'][k].relationName : ' '
+          });
+        } else if ( obj['links'][k].target === 330 && obj['links'][k].source > 330 ) {
+          fileToSave.relations.push({
+            'target' : 0 ,
+            'source' : obj['links'][k].source != null ? obj['links'][k].source : ' ',
+            'name' : (obj['links'][k].relationName != null && obj['links'][k].relationName.length > 0 ) ? obj['links'][k].relationName : ' '
+          });
+        } else if ( obj['links'][k].source === 330 && obj['links'][k].target > 330) {
+          fileToSave.relations.push({
+            'target' : obj['links'][k].target != null ? obj['links'][k].target  : ' ',
+            'source' : 0 ,
+            'name' : (obj['links'][k].relationName != null && obj['links'][k].relationName.length > 0 ) ? obj['links'][k].relationName : ' '
+          });
+        } else if (obj['links'][k].source <= 330 &&  obj['links'][k].target > 330 ) {
+          fileToSave.relations.push({
+            'target' : obj['links'][k].target != null ? obj['links'][k].target : ' ',
+            'source' : obj['links'][k].source != null ? obj['links'][k].source + 1 : ' ',
+            'name' : (obj['links'][k].relationName != null && obj['links'][k].relationName.length > 0 ) ? obj['links'][k].relationName : ' '
+          });
+        } else if (obj['links'][k].target <= 330 &&  obj['links'][k].source > 330 ) {
+          fileToSave.relations.push({
+            'target' : obj['links'][k].target != null ? obj['links'][k].target + 1 : ' ',
+            'source' : obj['links'][k].source != null ? obj['links'][k].source : ' ',
+            'name' : (obj['links'][k].relationName != null && obj['links'][k].relationName.length > 0 ) ? obj['links'][k].relationName : ' '
+          });
+        } else if (obj['links'][k].source <= 330 && obj['links'][k].target <= 330 ) {
+          fileToSave.relations.push({
+            'target' : obj['links'][k].target != null ? obj['links'][k].target + 1 : ' ',
+            'source' : obj['links'][k].source != null ? obj['links'][k].source + 1 : ' ',
+            'name' : (obj['links'][k].relationName != null && obj['links'][k].relationName.length > 0 ) ? obj['links'][k].relationName : ' '
+          });
+        } else {
+          fileToSave.relations.push({
+            'target' : obj['links'][k].target != null ? obj['links'][k].target : ' ',
+            'source' : obj['links'][k].source != null ? obj['links'][k].source : ' ',
+            'name' : (obj['links'][k].relationName != null && obj['links'][k].relationName.length > 0 ) ? obj['links'][k].relationName : ' '
+          });
+        }
+      });
+    } else {
+      return {'Error' : 'Invalid Format'};
+    }
+    if ( obj.hasOwnProperty('external_resources') ) {
+      Object.keys(obj['external_resources']).forEach( k => {
+        if (obj['external_resources'][k].nodes.length > 0) {
+          fileToSave.references.push({
+            'concepts' : obj['external_resources'][k].nodes,
+            'name' :  obj['external_resources'][k].title.length > 0 ?  obj['external_resources'][k].title : ' ',
+            'description' : obj['external_resources'][k].description.length > 0 ? obj['external_resources'][k].description : ' ',
+            'url' : obj['external_resources'][k].url.length > 0 ? obj['external_resources'][k].url : ' '
+          });
+        }
+      });
+    } else {
+      return {'Error' : 'Invalid Format'};
+    }
+    if ( obj.hasOwnProperty('learning_outcomes') ) {
+      Object.keys(obj[ 'learning_outcomes' ]).forEach( k => {
+        if (obj['learning_outcomes'][k].nodes.length > 0) {
+          fileToSave.skills.push({
+            'concepts': obj['learning_outcomes'][k].nodes,
+            'name': obj['learning_outcomes'][k].name.length > 0 ? obj['learning_outcomes'][k].name : ' ',
+          });
+        }
+      });
+    } else {
+      return {'Error' : 'Invalid Format'};
+    }
+    return fileToSave;
   }
 }
