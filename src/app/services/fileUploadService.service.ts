522bccbc4ce4fd10f8cf88d471d35ba28b62f054
pardoma <pardoma@uji.es>
2024-05-20 12:08:54 +0200

    fix: texts and little html details

--- src/app/services/fileUploadService.service.ts
+++ src/app/services/fileUploadService.service.ts
@@ -18,87 +18,6 @@ export class FileUploadServiceService {
 
   public allBoKs = null;
   public resp = {};
-  /*   uploadFile(file: any, user: any) { //, user: any
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      };
-      return this.currentVersion().pipe(
-        catchError(this.handleError)
-      ).subscribe((cversion) => {
-        file.creationYear = new Date().getFullYear();
-        const fileToSave = JSON.stringify(file);
-        const newVersion = (parseInt(cversion) + 1).toString();
-        file.version = newVersion;
-        file.updateDate = formatDate(new Date(), 'yyyy/MM/dd', 'en');
-        this.resp = file;
-        const currentFile = JSON.stringify(file);
-        // eo4geo-uji
-        const configUrl = this.URL_BASE + 'v' + newVersion + '.json?auth=' + user;
-        const currentUrl = this.URL_BASE + 'current.json?auth=' + user;
-        this.http.put(configUrl, fileToSave, httpOptions).pipe(
-          catchError(this.handleError)
-        ).subscribe(
-          res => this.resp = res,
-          err => this.resp = err,
-        );
-        this.http.put(currentUrl, currentFile, httpOptions).pipe(
-          catchError(this.handleError)
-        ).subscribe(
-          res => this.resp = res,
-          err => this.resp = err,
-        );
-        // findinbok
-        const configUrl1 = this.URL_BASE_BACKUP1 + 'v' + newVersion + '.json';
-        const currentUrl1 = this.URL_BASE_BACKUP1 + 'current.json';
-        this.http.put(configUrl1, fileToSave, httpOptions).pipe(
-          catchError(this.handleError)
-        ).subscribe(
-          res => this.resp = res,
-          err => this.resp = err,
-        );
-        this.http.put(currentUrl1, currentFile, httpOptions).pipe(
-          catchError(this.handleError)
-        ).subscribe(
-          res => this.resp = res,
-          err => this.resp = err,
-        );
-        // eo4geo-uji-backup
-        const configUrl2 = this.URL_BASE_BACKUP2 + 'v' + newVersion + '.json';
-        const currentUrl2 = this.URL_BASE_BACKUP2 + 'current.json';
-        this.http.put(configUrl2, fileToSave, httpOptions).pipe(
-          catchError(this.handleError)
-        ).subscribe(
-          res => this.resp = res,
-          err => this.resp = err,
-        );
-        this.http.put(currentUrl2, currentFile, httpOptions).pipe(
-          catchError(this.handleError)
-        ).subscribe(
-          res => this.resp = res,
-          err => this.resp = err,
-        );
-        // https://eo4geo-uji-backup2-default-rtdb.europe-west1.firebasedatabase.app/
-        const configUrl3 = this.URL_BASE_BACKUP3 + 'v' + newVersion + '.json';
-        const currentUrl3 = this.URL_BASE_BACKUP3 + 'current.json';
-        this.http.put(configUrl3, fileToSave, httpOptions).pipe(
-          catchError(this.handleError)
-        ).subscribe(
-          res => this.resp = res,
-          err => this.resp = err,
-        );
-        this.http.put(currentUrl3, currentFile, httpOptions).pipe(
-          catchError(this.handleError)
-        ).subscribe(
-          res => this.resp = res,
-          err => this.resp = err,
-        );
-      },
-        err => this.resp = err);
-  
-    }
-   */
 
   uploadNewBoK(file: any, token: any) {
     const httpOptions = {
@@ -122,8 +41,6 @@ export class FileUploadServiceService {
       this.allBoKs = cversions;
 
       const currentFile = JSON.stringify(cversions);
-
-      //  console.log(currentFile)
 
       const vUrl = this.URL_BASE + '.json?auth=' + token;
       this.http.put(vUrl, currentFile, httpOptions).pipe(
@@ -153,26 +70,6 @@ export class FileUploadServiceService {
       err => this.resp = err,
     );
   }
-
-
-
-
-  /*   uploadBoKAPIFile(newVersion, file: any) {
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json'
-        })
-      };
-      const fileToSave = JSON.stringify(file);
-      this.resp = file;
-      const configUrl = this.URL_BASE_BOKAPI + newVersion + '.json';
-      this.http.put(configUrl, fileToSave, httpOptions).pipe(
-        catchError(this.handleError)
-      ).subscribe(
-        res => this.resp = res,
-        err => this.resp = err,
-      );
-    } */
 
   // Get fullBoK
   fullBoK(): Observable<any> {
